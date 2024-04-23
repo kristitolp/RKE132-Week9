6912f1bd83b87bf4f2a976e7a24aa7a104d0f265
kristitolp <kristi.tolp@tktk.ee>
2024-04-23 20:27:16 +0300

    Week9 finish

---
+++ Test/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+using System.Collections.Generic;
+
+static void DisplayDataFromFile(List<string> someList)
+{
+    foreach (string line in someList)
+    {
+        Console.WriteLine(line);
+    }
+}
